aa37adc0ee9245759a3638191cc6585397f278bd
VladimirTitov4 <vtitov4@yandex.ru>
2019-05-22 22:38:55 +0400

    Дмитрий, немного доделал код к предыдущему (6му) уроку.
    Я просмотрел подробно код, который вы дали в качестве собственного решения.
    В целом в нем все понятно, но я хотел принципы, которые вы заложили у себя - добавить к себе, у меня не получилось с функцией проверки товара в корзине. Она не отрабатывается так как надо.
    
    Но нужно делать 7ДЗ поэтому оставлю так. А 7ДЗ буду делать уже как вы делали.

--- lesson-6/lesson-6-1.js
+++ lesson-6/lesson-6-1.js
@@ -1,43 +1,25 @@
 // Генерация блока Продуктов
 function buildProductsList(items) {
-    var $products = document.getElementById('products');
-    $products.textContent = '';
+    var $catalog = document.querySelector('#catalog')
+    $catalog.textContent = '';
 
     for (var i = 0; i < items.length; i++) {
-        var $goods = document.createElement('div');
+        var $goods = document.createElement('li');
         $goods.dataset.number = i;
         $goods.classList.add('goods');
-
-        var $img = document.createElement('img');
-        $img.src = items[i].img;
 
         var $title = document.createElement('h2');
         $title.textContent = items[i].name + '(' + items[i].price + 'руб) ';
 
         var $button = document.createElement('button');
         $button.classList.add('button');
-        $button.id = 'button';
         $button.textContent = 'купить';
 
+        $goods.classList.add('itemElement');
+        $goods.appendChild($title);
+        $goods.appendChild($button);
 
-        var $item = document.createElement('li');
-        $item.classList.add('itemElement')
-        $item.appendChild($img);
-        $item.appendChild($title);
-        $item.appendChild($button);
-        $goods.appendChild($item);
-        $products.appendChild($goods);
-    }
-}
-
-// Генерация отрисовки корзины
-function buildCartList() {
-    var $cart;
-    $cart.textContent = '';
-    for (var i = 0; i < cart.length; i++) {
-        var $goods = document.createElement('div');
-        $goods.dataset.number = i;
-
+        $catalog.appendChild($goods);
     }
 }
 
@@ -63,24 +45,38 @@ function countBasketPrice(cart) {
     $cart.textContent = message;
 }
 
-// Добавление товаров в корзину.
-function addGoodsToCart(goods) {
-    //goods.quantity = 1;
+function isExist(id) {
+    for (var i = 0; i < cart.length; i++) {
+        if (cart[i].id === id) {
+            return true;
+            console.log(cart[i].id);
+        }
+    }
+}
+
+function addToCart(goods) {
+    goods.quantity = 1;
     cart.push(goods);
 }
 
 function handleButtonClick(event) {
-    addGoodsToCart(products[event.target.parentNode.dataset.number]);
-    init();
+    if (event.target.textContent === 'купить') {
+        if (isExist(event.target.parentNode.dataset.number)) {
+            console.log('Такой товар в корзине есть');
+        } else {
+            addToCart(products[event.target.parentNode.dataset.number]);
+            countBasketPrice(cart);
+            console.log('Такого товара в корзине нет');
+
+        }
+    }
 }
 
 function init() {
     buildProductsList(products);
-    countBasketPrice(cart)
-    var $products = document.querySelector('#products');
-    $products.addEventListener('click', handleButtonClick);
-    var $cart = document.querySelector('#cart');
-    $cart.addEventListener('click', handleButtonClick);
+
+    var $catalog = document.querySelector('#catalog');
+    $catalog.addEventListener('click', handleButtonClick);
     }
 
 window.addEventListener('load', init);
@@ -107,32 +103,33 @@ window.addEventListener('load', init);
 
 
 
+var cart = [];
 
 
 
 var products = [
     {
-        img: 'http://sdtekdirect.com/thumbs/az/sdtek/new-glass/GLASS-I7Q3/4.jpg',
+        id: 1,
         name: 'iPhone',
         price: 70000,
+        quantity: 0,
     },
     {
-        img: 'http://baraden.ru/media/catalog/product/cache/1/small_image/295x295/9df78eab33525d08d6e5fb8d27136e95/x/8/x8628d92015b03ed287eb76fba0af06ba.jpg.pagespeed.ic_.vmhv1lktdx1.jpg',
+        id: 2,
         name: 'Защитная пленка',
         price: 1500,
+        quantity: 0,
     },
     {
-        img: 'https://vn-test-11.slatic.net/p/2/pin-sac-du-phong-xiaomi-10000mah-gen-2-new-2018-5105-60805192-bfe2be41d6bb61f2900a7906a18faa89-catalog.jpg_120x120q75.jpg',
+        id: 3,
         name: 'Power bank',
         price: 5000,
+        quantity: 0,
     },
 ];
 
 
-var cart = [];
 
-//buildProductsList(products);
-//countBasketPrice(cart);
 
 
 
